f0ad2e688e75e4006f59d773062f377a020a06cb
elizakuz <kizminaeliz@gmail.com>
2015-04-26 07:30:43 +0300

    task 40

---
+++ ring/src/app/Program.fs
@@ -0,0 +1,15 @@
+﻿module Modul
+
+type ModulBuilder (n : int) =
+    member this.Bind (m, f) = f (m % n)
+    member this.Return x =
+        let rec f y =
+            if y < 0 then f (y + n)
+            else y
+        f (x % n)
+
+let modul x = new ModulBuilder (x)
+
+[<EntryPoint>]
+let main argv =
+    0
