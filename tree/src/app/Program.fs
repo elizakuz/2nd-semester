b460eca8a33ae2b831bef2bb643381220a1d6ca3
elizakuz <kizminaeliz@gmail.com>
2015-04-26 09:55:26 +0300

    task 41

---
+++ tree/src/app/Program.fs
@@ -0,0 +1,39 @@
+﻿module tree
+
+type Tree = Nil | T of Tree * int * Tree
+
+type TreeBuilder () =
+    member this.Bind (x, f) =
+        match x with
+        | Nil -> Nil
+        | T (a, b, c) -> T (this.Bind (a, f), f b, this.Bind (c, f))
+    member this.Combine (x, y) =
+        match x, y with
+        | Nil, _ -> y
+        | _, Nil -> x  
+        | T (a, b, c), T (k, l, m) -> if b < l then T (a, b, this.Combine (c, y))
+                                      else T (this.Combine (x, k), l, m)
+    member this.Return x = 
+        T (Nil, x, Nil)
+    member this.ReturnFrom x = x
+    member this.For (x, f) = this.Bind (x, f)
+
+let tree = new TreeBuilder()
+
+let map f tr =
+    tree {
+        for x in tr do
+             return! f x
+    }
+
+let rec filter f tr =
+    tree {
+        match tr with
+        | Nil -> return! Nil
+        | T (a, b, c) -> if (f b) then return! T (filter f a, b, filter f c) 
+                         else return! filter f a
+    }
+
+[<EntryPoint>]
+let main argv =
+    0      
---
+++ tree/src/test/Test.fs
@@ -0,0 +1,37 @@
+﻿module test
+
+open tree
+open NUnit.Framework
+
+[<Test>]
+let ``combineTest1`` () =
+    let tr = T (Nil, 1, Nil)
+    let t = T (Nil, 2, Nil)
+    Assert.AreEqual (tree.Combine (tr, t), T (Nil, 1, T (Nil, 2, Nil)))
+
+[<Test>]
+let ``combineTest free brunch`` () =
+    let tr = T (T (Nil, 1, Nil), 3, Nil)
+    let t = T (Nil, 2, Nil)
+    Assert.AreEqual (tree.Combine (tr, t), T (T (T (Nil, 1, Nil), 3, Nil), 2, Nil))
+
+[<Test>]
+let ``mapTest1 Nil`` () =
+    let tr = Nil
+    let result = Nil
+    Assert.AreEqual (map ((+) 1) tr, result)
+
+[<Test>]
+let ``mapTest2 * 2`` () =
+    let tr = T (T (Nil, 1, Nil), 5, T (Nil, 8, Nil))
+    let result = T (T (Nil, 2, Nil), 10, T (Nil, 16, Nil))
+    Assert.AreEqual (map (fun x -> x * 2) tr, result)
+
+[<Test>]
+let ``filterTest < 6`` () =
+    let tr = T (T (Nil, 1, T (Nil, 3, Nil)), 5, T (Nil, 8, T (Nil, 9, Nil)))
+    let result = T (T (Nil, 1, T (Nil, 3, Nil)), 5, Nil)
+    Assert.AreEqual (filter (fun x -> x < 6) tr, result)
+
+
+
