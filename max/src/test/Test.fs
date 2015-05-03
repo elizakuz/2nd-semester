bd84e6cfed37e28059a1de2ec88077f44181fe26
elizakuz <kizminaeliz@gmail.com>
2015-05-03 06:59:28 +0300

    task46

---
+++ max/src/test/Test.fs
@@ -0,0 +1,16 @@
+﻿module test
+
+open max
+open NUnit.Framework
+
+[<TestCase (1, [|1;1;1;1;1;1;1;1;123;1;1;1;1;1;1|], Result = 123)>]
+[<TestCase (2, [|1;1;1;1;1;1;1;1;123;1;1;1;1;1;1|], Result = 123)>]
+[<TestCase (3, [|1;1;1;1;1;1;1;1;123;1;1;1;1;1;1|], Result = 123)>]
+[<TestCase (15, [|1;1;1;1;1;1;1;1;123;1;1;1;1;1;1|], Result = 123)>]
+let test i arr = max i arr
+
+[<TestCase (1, [|1|], Result = 1)>]
+[<TestCase (2, [|1|], Result = 1)>]
+[<TestCase (10, [|1|], Result = 1)>]
+let ``Only one elem`` i arr = max i arr
+
